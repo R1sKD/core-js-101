d75752402f9687115b7e9acf4bde8dacd8461f42
Denis Prokhorenko <denis07011997q@gmail.com>
2022-01-04 23:06:31 +0300

    feat: solve 04-date-tasks (5 / 5)

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -104,8 +104,12 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours() % 12;
+  const minutes = date.getMinutes();
+  let angle = Math.abs(0.5 * (60 * hours + minutes) - 6 * minutes);
+  angle = angle > 180 ? angle - 180 : angle;
+  return angle * (Math.PI / 180);
 }
 
 
